fc5c533ddd1b8556bf0f0ae386423ba1bf4c1e59
Momin Riyadh <mominriyadh@gmail.com>
2022-07-24 09:51:28 +0600

    fix and deploy

--- mern-complete-tutorial/my-app/src/components/ShowBookList.js
+++ mern-complete-tutorial/my-app/src/components/ShowBookList.js
@@ -47,7 +47,7 @@ class ShowBookList extends Component {
                             <h2 className="display-4 text-center">Books List</h2>
                         </div>
 
-                        <div className="col-md-11">
+                        <div className="col-md-12">
                             <Link to="/create-book" className="btn btn-outline-warning float-right">
                                 + Add New Book
                             </Link>
