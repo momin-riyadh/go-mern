d718a8fa5e1dc0d27c82531d76465db526ff5258
Momin Riyadh <mominriyadh@gmail.com>
2022-07-25 13:32:35 +0600

    fix and deploy

--- mern-self/client/src/App.js
+++ mern-self/client/src/App.js
@@ -1,21 +1,21 @@
 
 import React, {Component} from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter, Routes,  Route} from "react-router-dom";
 import './App.css';
 import CreateBook from "./components/CreateBook";
-import showBookList from "./components/ShowBookList";
+import ShowBookList from "./components/ShowBookList";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-            <Route exact path="/" component={showBookList} />
-            <Route path={'/create-book'} component={CreateBook} />
+      <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<ShowBookList/>} />
+            <Route path={'/create-book'} element={<CreateBook/>} />
             {/*<Route path={'/create-book'} component={CreateBook} />*/}
 
-        </div>
-      </Router>
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
