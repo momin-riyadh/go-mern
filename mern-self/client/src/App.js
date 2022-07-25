6ee281367960b916edef58feaa378279e49ee138
Momin Riyadh <mominriyadh@gmail.com>
2022-07-25 10:10:30 +0600

    fix and deploy

--- mern-self/client/src/App.js
+++ mern-self/client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+
