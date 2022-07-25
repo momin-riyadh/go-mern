f03718a98f097a7ff49c429ae47ddec966548a42
Momin Riyadh <mominriyadh@gmail.com>
2022-07-25 10:32:06 +0600

    fix and deploy

--- mern-self/client/src/components/CreateBook.js
+++ mern-self/client/src/components/CreateBook.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from "axios";
 
 class CreateBook extends Component {
     constructor() {
@@ -12,11 +13,53 @@ class CreateBook extends Component {
             publisher: '',
         }
     }
+
+    onChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const data = {
+            title: this.state.title,
+            isbn: this.state.isbn,
+            author: this.state.author,
+            description: this.state.description,
+            published_date: this.state.published_date,
+            publisher: this.state.publisher,
+        }
+
+        axios.post('http://localhost:5000/api/books', data)
+            .then(res => {
+                this.setState({
+                    title: '',
+                    isbn: '',
+                    author: '',
+                    description: '',
+                    published_date: '',
+                    publisher: '',
+                })
+                this.props.history.push('/');
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+                <div className="CreateBook">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8 m-auto">
+                                <link to='/' className="btn btn-outline-warning float-left">
+                                    Show Book List
+                                </link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
